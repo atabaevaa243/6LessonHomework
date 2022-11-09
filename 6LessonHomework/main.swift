5024c671635142e2d78c9961198d8e27a887e23d
Aziza <atabaevaa243@gmail.com>
2022-11-09 18:38:01 +0600

    edit

--- 6LessonHomework/main.swift
+++ 6LessonHomework/main.swift
@@ -8,22 +8,22 @@ import Foundation
 //Второй - элементы
 //Задача: При вводе чисел либо строк в readLine(неограниченное количество) отсортировать и распределить по нужным массивам
 
-var arrayMain : [[String]] = [[],[]]
-var readline = ""
-var stop = false
-while stop == false{
-    readline = readLine()!
-    if readline.count == 0{
-        stop = true
-    }else{
-        if Int(readline) ?? 0 != 0 || Int(readline) ?? 12 == 0{
-            arrayMain[1].append(readline)
-        }else{
-            arrayMain[0].append(readline)
-        }
-    }}
-
-print(arrayMain)
+//var arrayMain : [[String]] = [[],[]]
+//var readline = ""
+//var stop = false
+//while stop == false{
+//    readline = readLine()!
+//    if readline.count == 0{
+//        stop = true
+//    }else{
+//        if Int(readline) ?? 0 != 0 || Int(readline) ?? 12 == 0{
+//            arrayMain[1].append(readline)
+//        }else{
+//            arrayMain[0].append(readline)
+//        }
+//    }}
+//
+//print(arrayMain)
 
 //#2. Создать двухмерный массив
 //Первый слой - числа от 1 до 100
@@ -31,4 +31,22 @@ print(arrayMain)
 //Задача: Создать массив с разными словами и в цикле добавить рандомно слова в количестве первого слоя в массив по текущему номеру
 
 
+var array2d = [[String]]()
+var stringArray = ["apple", "table", "macbook", "clock", "pen"]
 
+for i in 1...100{
+    array2d.append([String(i)])
+}
+
+for (index, item) in array2d.enumerated(){
+    if Int(item[0])! > 1{
+        for _ in 1...Int(item[0])!{
+            array2d[index].append(stringArray[Int.random(in: 0...stringArray.count - 1)])
+        }
+    }else{
+        array2d[index].append(stringArray[Int.random(in: 0...stringArray.count - 1)])
+    }
+    
+}
+
+print(array2d)
